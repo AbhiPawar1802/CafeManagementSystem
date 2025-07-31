3b7a2ef6cfb4328f99cf180d1a6bd8f1541af312
AbhishekPawar1802 <pabhi3195@gmail.com>
2025-07-31 19:03:45 +0530

    Renamed backend folder for render compatibility

--- Cafe Management backend/com.inn.cafe/src/main/java/com/inn/cafe/dao/ReservationDao.java
+++
@@ -1,9 +0,0 @@
-package com.inn.cafe.dao;
-
-import com.inn.cafe.POJO.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservationDao extends JpaRepository<Reservation, Integer> {
-}
